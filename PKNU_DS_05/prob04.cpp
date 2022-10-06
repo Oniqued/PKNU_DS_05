a42dbfa1bfafdd1ffd86d8836edc7b7b4f12d250
Oniqued <zktl3575@gmail.com>
2022-10-06 15:00:07 +0900

    [prob04.cpp] second commit

--- PKNU_DS_05/prob04.cpp
+++ PKNU_DS_05/prob04.cpp
@@ -5,12 +5,17 @@
 #define BUFFER_SIZE 256
 
 void load();
+void filter(char buffer[]);
 
 char buffer[BUFFER_SIZE];
+char* content[BUFFER_SIZE];
 
 int main() {
 
 	load();
+	//for (int i = 0; content[i] != NULL; i++) {
+	//	printf("%s\n", content[i]);
+	//}
 
 	return 0;
 }
@@ -21,7 +26,39 @@ void load() {
 		printf("ERROR! FAILED TO OPEN FILE\n");
 		return;
 	}
+	int i = 0; //줄의 번호 저장
 	while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
-		printf("%s", buffer);
+		//문장 끝에있는 개행 제거
+		for (int j = 0; j < BUFFER_SIZE; j++) {
+			if (buffer[j] == '\n') {
+				buffer[j] = NULL;
+			}
+		}
+		//Tag를 제거
+		filter(buffer);
+		break;
+		//Tag를 제거한 한 줄
+		content[i] = strdup(buffer);
+		i++;
 	}
+}
+
+void filter(char buffer[]) {
+	int i = 0;
+	int j = 0;
+	char tmp[BUFFER_SIZE];
+	while (buffer[i] != NULL) {
+		if (buffer[i] == '<') {
+			//Tag의 끝부분이 나올때 까지 i++
+			while (buffer[i] != '>') {
+				printf("%d ", i);
+				i++;
+			}
+		}
+		tmp[j] = buffer[i];
+		j++;
+	}
+	tmp[j] = NULL;
+	strcpy(buffer, tmp);
+	printf("%s", buffer);
 }
